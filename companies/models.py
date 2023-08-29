79688887b8f22f02245084e6cf1768de66636cf3
christopherece <christopheranchetaece@gmail.com>
2023-08-29 21:12:13 +1200

    Added title Field

--- companies/models.py
+++ companies/models.py
@@ -5,7 +5,9 @@ class Company(models.Model):
     name = models.CharField(max_length=200)
     hire_date = models.DateField(blank=True)
     end_date = models.DateField(blank=True, null=True)
+    title = models.CharField(max_length=200,blank=True, null=True)
     logo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+
 
     def __str__(self):
         return self.name
