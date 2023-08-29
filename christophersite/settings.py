ddb11cbb48945fe3b16eaec42fd2a7cdb15a1b56
christopherece <christopheranchetaece@gmail.com>
2023-08-29 21:29:49 +1200

    Added CSRF

--- christophersite/settings.py
+++ christophersite/settings.py
@@ -64,6 +64,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'christophersite.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://info.christopherancheta.co.nz'
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
