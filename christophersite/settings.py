5fb9e07dbb3992390a9948b2543b882383bb13d6
christopherece <christopheranchetaece@gmail.com>
2023-08-29 21:18:01 +1200

    Moved db to PSQL

--- christophersite/settings.py
+++ christophersite/settings.py
@@ -69,9 +69,17 @@ WSGI_APPLICATION = 'christophersite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default2': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'christophersite_tbl',
+        'USER': 'postgres',
+        'PASSWORD': '!pass1234',
+        'HOST':'192.168.10.225'
     }
 }
 
